759d225edddd361de6410a44114710ed959e76bf
Mitch <mitchjoelklein@hotmail.com>
2025-03-17 11:11:56 +1100

    wishlist button

--- src/components/productList.tsx
+++ src/components/productList.tsx
@@ -2,16 +2,30 @@
 
 import { useDispatch, useSelector } from "react-redux";
 import productDataObjects from "../data/products";
-// import { CheckCircleIcon } from "@heroicons/react/20/solid";
+import { HeartIcon } from "@heroicons/react/20/solid";
+import { HeartIcon as HeartOutlineIcon } from "@heroicons/react/24/outline";
+
 import cartSlice from "../redux/reducers/cartReducer";
+import wishlistSlice from "@/redux/reducers/wishlistReducer";
 import { RootState } from "../redux/store";
 
 export default function ProductList() {
   const { cartProducts } = useSelector(
     (state: RootState) => state.reducer.cart
   );
+  const { wishlistProducts } = useSelector(
+    (state: RootState) => state.reducer.wishlist
+  );
+  const { addToWishlist, removeFromWishlist } = wishlistSlice.actions;
   const { addToCart, removeFromCart } = cartSlice.actions;
   const dispatch = useDispatch();
+
+  {
+    /* <a href="#">
+                    <span aria-hidden="true" className="absolute inset-0" />
+                    {product.name}
+                  </a> */
+  }
 
   console.log("cartProductIds", cartProducts);
 
@@ -31,58 +45,92 @@ export default function ProductList() {
                 src={product.image.src}
                 className="aspect-3/4 w-full bg-gray-200 object-cover sm:aspect-auto sm:h-96"
               />
-              <div className="flex flex-1 flex-col space-y-2 p-4">
-                <h3 className="text-sm font-medium text-gray-900">
-                  {/* <a href="#">
-                    <span aria-hidden="true" className="absolute inset-0" />
+              <div className="flex flex-1 justify-between p-4">
+                {/* Left side - Product information */}
+                <div className="flex flex-col gap-1">
+                  <h3 className="text-sm font-medium text-gray-900">
                     {product.name}
-                  </a> */}
-                  {product.name}
-                </h3>
-                <p className="text-sm text-gray-500">{product.description}</p>
-                <div className="flex flex-1 flex-col justify-end">
-                  {/* <p className="text-sm text-gray-500 italic">
-                    {product.options}
-                  </p> */}
-                  <p className="text-base font-medium text-gray-900">
-                    {product.price}
+                  </h3>
+                  <p className="text-sm text-gray-500">{product.description}</p>
+                  <p className="text-sm font-medium text-gray-900">
+                    ${product.price}
                   </p>
                 </div>
-                <div>
-                  {!cartProducts.some(
-                    (cartItem) => cartItem.id === product.id
-                  ) && (
-                    <button
-                      type="button"
-                      onClick={() =>
-                        dispatch(
-                          addToCart({ id: product.id, price: product.price })
-                        )
-                      }
-                      className="rounded-sm bg-white px-2 py-1 text-xs font-semibold text-gray-900 ring-1 shadow-xs ring-gray-300 ring-inset hover:bg-gray-50 cursor-pointer"
-                    >
-                      Add to cart
-                    </button>
-                  )}
 
-                  {cartProducts.some(
-                    (cartItem) => cartItem.id === product.id
-                  ) && (
-                    <button
-                      type="button"
-                      onClick={() =>
-                        dispatch(
-                          removeFromCart({
-                            id: product.id,
-                            price: product.price,
-                          })
-                        )
-                      }
-                      className="rounded-sm bg-white px-2 py-1 text-xs font-semibold text-gray-900 ring-1 shadow-xs ring-gray-300 ring-inset hover:bg-gray-50 cursor-pointer"
-                    >
-                      Remove from cart
-                    </button>
-                  )}
+                {/* Right side - Wish and cart button */}
+                <div className="flex flex-col justify-between items-end h-full">
+                  <div>
+                    {!wishlistProducts.some(
+                      (wishlistItem) => wishlistItem.id === product.id
+                    ) && (
+                      <button
+                        type="button"
+                        onClick={() =>
+                          dispatch(
+                            addToWishlist({
+                              id: product.id,
+                            })
+                          )
+                        }
+                        className="cursor-pointer"
+                      >
+                        <HeartOutlineIcon className="h-6 w-6 text-amber-900" />{" "}
+                      </button>
+                    )}
+                    {wishlistProducts.some(
+                      (wishlistItem) => wishlistItem.id === product.id
+                    ) && (
+                      <button
+                        type="button"
+                        onClick={() =>
+                          dispatch(
+                            removeFromWishlist({
+                              id: product.id,
+                            })
+                          )
+                        }
+                        className="cursor-pointer"
+                      >
+                        <HeartIcon className="h-6 w-6 text-amber-900" />{" "}
+                      </button>
+                    )}
+                  </div>
+                  <div>
+                    {!cartProducts.some(
+                      (cartItem) => cartItem.id === product.id
+                    ) && (
+                      <button
+                        type="button"
+                        onClick={() =>
+                          dispatch(
+                            addToCart({ id: product.id, price: product.price })
+                          )
+                        }
+                        className="rounded-sm bg-white px-2 py-1 text-xs font-semibold text-gray-900 ring-1 shadow-xs ring-gray-300 ring-inset hover:bg-gray-50 cursor-pointer"
+                      >
+                        Add to cart
+                      </button>
+                    )}
+
+                    {cartProducts.some(
+                      (cartItem) => cartItem.id === product.id
+                    ) && (
+                      <button
+                        type="button"
+                        onClick={() =>
+                          dispatch(
+                            removeFromCart({
+                              id: product.id,
+                              price: product.price,
+                            })
+                          )
+                        }
+                        className="rounded-sm bg-white px-2 py-1 text-xs font-semibold text-gray-900 ring-1 shadow-xs ring-gray-300 ring-inset hover:bg-gray-50 cursor-pointer"
+                      >
+                        Remove from cart
+                      </button>
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
--- src/redux/Product.ts
+++ src/redux/Product.ts
@@ -1,18 +1,18 @@
-export interface Product {
-  name: string;
-  description: string;
-  price: number;
-  image?: unknown;
-}
+// export interface Product {
+//   name: string;
+//   description: string;
+//   price: number;
+//   image?: unknown;
+// }
 
-export interface ProductsState {
-  products: Product[];
-  status: "idle" | "loading" | "succeeded" | "failed";
-  error: string | null;
-}
+// export interface ProductsState {
+//   products: Product[];
+//   status: "idle" | "loading" | "succeeded" | "failed";
+//   error: string | null;
+// }
 
-export const initialState: ProductsState = {
-  products: [],
-  status: "idle",
-  error: null,
-};
+// export const initialState: ProductsState = {
+//   products: [],
+//   status: "idle",
+//   error: null,
+// };
